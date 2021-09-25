1131211647f9515115bbf557e5aa63c844aae820
geehaad <gehadhisham7@gmail.com>
2021-09-25 14:38:47 +0200

    Delete 977. Squares of a Sorted Array.py

--- 14 Days Study Plan to Crack Algo/Algorithm1/Day2_TwoPinters/977. Squares of a Sorted Array.py
+++
@@ -1,8 +0,0 @@
-class Solution(object):
-    def sortedSquares(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[int]
-        """
-        return sorted([i*i for i in nums])
-    
